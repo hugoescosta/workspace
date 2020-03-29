20a05ffd8884562b749a9b0b591ee2c6822e14bc
Hugo Costa <hugo.edu@gmail.com>
2020-03-29 10:36:30 -0300

    TESTE02 Capitulo07

---
+++ Livro_Deitel_Java08/src/Capitulo07/teste2.java
@@ -0,0 +1,12 @@
+package Capitulo07;
+
+import javax.swing.JOptionPane;
+
+public class teste2 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Teste de Mensagem");
+
+	}
+
+}
